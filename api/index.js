01860983f348c2a905eeeabcee78709b7cb060a3
LongDayJ <juuarezjose92@gmail.com>
2026-06-19 10:37:47 -0300

    remoção de preflight

--- api/index.js
+++ api/index.js
@@ -1,6 +1,15 @@
 const { createApp } = require("../dist/lambda");
 
 module.exports = async (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+    if (req.method === "OPTIONS") {
+        res.status(204).end();
+        return;
+    }
+
     const app = await createApp();
     app(req, res);
 };
